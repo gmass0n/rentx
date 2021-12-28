f0b52a155b73b68201e8c51514cfca468dfb5a5b
Gabriel Masson <g.masson010@gmail.com>
2021-12-28 20:20:08 -0300

    fix: resolver errors

---
+++ ios/rentx-Bridging-Header.h
@@ -0,0 +1,20 @@
+//
+//  rentx-Bridging-Header.h
+//  rentx
+//
+//  Created by Gabriel Masson on 28/12/21.
+//
+
+#ifndef rentx_Bridging_Header_h
+#define rentx_Bridging_Header_h
+
+
+#endif /* rentx_Bridging_Header_h */
+
+#import <React/RCTBundleURLProvider.h>
+#import <React/RCTRootView.h>
+#import <React/RCTViewManager.h>
+#import <React/RCTBridgeModule.h>
+
+// Silence warning
+#import "../../node_modules/@nozbe/watermelondb/native/ios/WatermelonDB/SupportingFiles/Bridging.h"
